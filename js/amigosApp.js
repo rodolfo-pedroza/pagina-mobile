e4544033413e2a1126f43b9d47624dd9032b751f
rodolfo-pedroza <rudy_pedroza@hotmail.com>
2021-11-15 13:03:38 -0600

    pagina de amigos lista

---
+++ js/amigosApp.js
@@ -0,0 +1,41 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            nombre: '',
+            email: '',
+            amigos: [
+                {id: 1, nombre: 'rodo', correo: 'ejemplo@gmail.com'},
+                {id: 2, nombre: 'arturo', correo: 'ejemplo1@gmail.com'},
+                {id: 3, nombre: 'rodrigo', correo: 'ejemplo2@gmail.com'},
+                {id: 4, nombre: 'melissa', correo: 'ejemplo3@gmail.com'},
+                {id: 5, nombre: 'andrea', correo: 'ejemplo4@gmail.com'},
+                {id: 6, nombre: 'daniel', correo: 'ejemplo5@gmail.com'}
+            ]
+        }
+    },
+    methods: {
+        agregarAmigo: function (nombre,email){
+            if(this.amigos.length==0){
+                lastId = 0
+            }else{
+                last = this.amigos[this.amigos.length -1]
+                lastId = last.id
+            }
+            this.amigos.push({id: lastId+=1, nombre: nombre, correo: email})
+            this.nombre= ''
+            this.email= ''
+        },
+        eliminarAmigo: function(numero){
+            numero -= 1
+            this.amigos.splice(numero,1)
+            for (var i = numero; i < this.amigos.length; i++) {
+                if(i <= this.amigos[i].id){ 
+                    this.amigos[i].id-=1
+                }         
+            }
+            
+        }
+    }
+})
+
+app.mount('#app')
